428f1dc5cfc387b3ab8d9bf45a00c6f54906007a
Tanmay Shrivastava <34827740+iamtanmayshrivastava@users.noreply.github.com>
2025-11-25 17:42:30 +0530

    Time: 3 ms (81.13%), Space: 18.5 MB (89.82%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #when input array is sorted use two poiner approach
         left, right = 0, len(numbers) - 1
         while left < right:
             s = numbers[left] + numbers[right]
