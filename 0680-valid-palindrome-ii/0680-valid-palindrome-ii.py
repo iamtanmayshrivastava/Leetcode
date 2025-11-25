2e2965106305d580c36209d4a9836e2d6553d131
Tanmay Shrivastava <34827740+iamtanmayshrivastava@users.noreply.github.com>
2025-11-25 14:58:01 +0530

    Time: 67 ms (33.91%), Space: 18 MB (34.85%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
 
         def check(l, r):
-            left, right = l,r  # check if substring s[l:r+1] is palindrome
+            left, right = l,r  # check if substring is palindrome
             while left < right:
                 if s[left] != s[right]:
                     return False
