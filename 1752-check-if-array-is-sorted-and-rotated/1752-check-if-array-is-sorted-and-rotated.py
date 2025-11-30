26d37277165d3a9abdec7dc688f96ec33eeae28a
Tanmay Shrivastava <34827740+iamtanmayshrivastava@users.noreply.github.com>
2025-11-30 16:10:45 +0530

    Time: 0 ms (100%), Space: 18 MB (16.74%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,11 @@
 class Solution:
-    def check(self, nums: List[int]) -> bool: 
+    def check(self, nums: List[int]) -> bool:
         n = len(nums)
-        if n == 1:
+        if n == 1: ## edge case -- if only 1 element then its sorted and rotated
             return True
         count = 0
         for i in range(n):
             if nums[i] > nums[(i+1)%n]:
                 count = count + 1
-        return count <=1
-        
+        return count <= 1
         
