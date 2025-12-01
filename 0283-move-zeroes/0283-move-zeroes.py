7d091161024050e9b9420f953ff09208aca5b875
Tanmay Shrivastava <34827740+iamtanmayshrivastava@users.noreply.github.com>
2025-12-01 21:17:06 +0530

    Time: 4 ms (60.24%), Space: 18.8 MB (49.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,9 +5,18 @@ class Solution:
 
         """
         n = len(nums)
+        i = 0
         j = 0
-        for i in range(n):
-            if nums[i]!= 0:
-                nums[j],nums[i] = nums[i],nums[j]
+
+        while i < n:
+            if nums[i] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
                 j = j+1
+            i= i+1
+
+        # j = 0
+        # for i in range(n):
+        #     if nums[i]!= 0:
+        #         nums[j],nums[i] = nums[i],nums[j]
+        #         j = j+1
         
